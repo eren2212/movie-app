5f8a81e97154ce4e52e2984789ab7dcd7548c411
eren iridere <eiridere@gmail.com>
2025-08-21 13:24:58 +0300

    create project and configure tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        ümit iridere
+      </h1>
+    </div>
+  );
+}
+
+export default App;
