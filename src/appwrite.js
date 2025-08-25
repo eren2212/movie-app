129071b163e879bc65d33724835f465215b70da9
eren iridere <eiridere@gmail.com>
2025-08-26 00:16:37 +0300

    Configure Appwrite database integration and update search count logic in movie app

--- src/appwrite.js
+++ src/appwrite.js
@@ -11,22 +11,21 @@ const database = new Databases(client);
 
 export const updateSearchCount = async (searchTerm, movie) => {
   try {
-    const response = database.listDocuments(
-      DATABASE_ID,
-      COLLECTION_ID,
-      Query.equal(searchTerm, searchTerm)
-    );
-
-    if ((await response.documents) > 0) {
+    const response = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.equal("searchTerm", [searchTerm]),
+    ]);
+    console.log(response);
+    if (response.documents.length > 0) {
       const doc = response.documents[0];
-      const newCount = newCount + 1;
-
-      database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
+      const newCount = doc.count + 1;
+      console.log(doc);
+      await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
         count: newCount,
       });
+      return;
     }
 
-    database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+    await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
       searchTerm,
       count: 1,
       poster_path: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`,
